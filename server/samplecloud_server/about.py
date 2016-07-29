2a9942f369dcbe3850885c9873d22889ae3c7f4b
Pauli Huttunen <pauli.huttunen@outlook.com>
2016-07-30 02:49:51 +0300

    redo about module viewer

--- server/samplecloud_server/about.py
+++ server/samplecloud_server/about.py
@@ -3,19 +3,25 @@ from flask_restful import Resource
 
 import package_about, system_about
 
-modules = {
+MODULES = {
     "package": package_about.about,
     "system": system_about.about
 }
 
-class CategoryView(Resource):
+class Viewer(Resource):
 
-    def get(self, module, category):
+    def get(self, module = None, category = None, value = None):
 
-        return modules[module][category]
+        if module:
+            data = MODULES[module]
 
-class ValueView(Resource):
+            if category:
+                data = data[category]
 
-    def get(self, module, category, value):
+                if value:
+                    data = data[value]
 
-        return modules[module][category][value]
+        else:
+            data = list(MODULES.keys())
+
+        return data
--- server/samplecloud_server/main.py
+++ server/samplecloud_server/main.py
@@ -4,17 +4,12 @@ from flask import Flask
 from flask_restful import Api
 
 import about
-import samples
 
-app = Flask("samplecloud-server")
+app = Flask("samplecloud_server")
 api = Api(app)
 
 # About API components
-api.add_resource(about.CategoryView, "/about/<category>")
-api.add_resource(about.ValueView, "/about/<category>/<value>")
-
-# Samples API components
-api.add_resource(samples.Find, "/data/samples/find")
-api.add_resource(samples.Info, "/data/samples/info")
-api.add_resource(samples.Download, "/data/samples/download")
-api.add_resource(samples.Upload, "/data/samples/upload")
+api.add_resource(about.Viewer, "/about",
+                                "/about/<module>",
+                                "/about/<module>/<category>",
+                                "/about/<module>/<category>/<value>")
